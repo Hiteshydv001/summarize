aa15de03f080f019e11ea62ce9402392b05799aa
HITESH KUMAR <114931638+Hiteshydv001@users.noreply.github.com>
2025-04-08 14:40:44 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -194,4 +194,4 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 if __name__ == "__main__":
     import uvicorn
     logger.info("Starting Uvicorn server...")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
